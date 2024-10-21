d13937998297f23803765c2b347c12a88b67eaa0
nicofrago <nicofrago@hotmail.com>
2024-10-21 10:59:15 +0100

    Refactor: implementing diagonal printing of Hello World

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,8 @@
-print("Hello World")
+sentence = 'Hello World'
+space = ' '
+for letter in sentence:
+    print(space, letter)
+    space+='   '
+
+print("Python and Machine Learning, Imperial College London")
 # import something 
